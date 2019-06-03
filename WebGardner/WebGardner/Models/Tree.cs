9cde4391089a560b30f8ed6b0deddc36eddce58c
florent417 <flole@live.dk>
2019-06-03 23:16:18 +0200

    Filled Models again

--- WebGardner/WebGardner/Models/Tree.cs
+++ WebGardner/WebGardner/Models/Tree.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace WebGardner.Models
 {
     public class Tree
     {
+        public int TreeId { get; set; }
+        [DisplayName("Sort")]
+        public string SortName { get; set; }
+        public int Amount { get; set; }
+
+
+        public Location Location { get; set; }
+        public int LocationId { get; set; }
     }
 }
