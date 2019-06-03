9fd7df739fcdf5f0b7aae83698ba6d0407680ed8
florent417 <flole@live.dk>
2019-06-03 17:47:22 +0200

    Added web Application

---
+++ WebGardner/WebGardner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebGardner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
