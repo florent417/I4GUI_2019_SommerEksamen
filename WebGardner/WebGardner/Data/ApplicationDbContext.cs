7833ddde0c3c94709d80fc46f26922cd70c75623
florent417 <flole@live.dk>
2019-06-03 21:22:48 +0200

    AppContext updated

--- WebGardner/WebGardner/Data/ApplicationDbContext.cs
+++ WebGardner/WebGardner/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebGardner.Models;
 
 namespace WebGardner.Data
 {
@@ -11,6 +12,21 @@ namespace WebGardner.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Tree>()
+                .HasOne(l => l.Location)
+                .WithMany(t => t.Trees)
+                .HasForeignKey(l => l.LocationId);
+
+            builder.Entity<Sensor>()
+                .HasOne(l => l.Location)
+                .WithMany(s => s.Sensors)
+                .HasForeignKey(l => l.LocationId);
         }
     }
 }
---
+++ WebGardner/WebGardner/Data/Migrations/20190603192147_InititalCreate.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebGardner.Data.Migrations
+{
+    public partial class InititalCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    LocationId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    LocationName = table.Column<string>(nullable: true),
+                    StreetName = table.Column<string>(nullable: true),
+                    StreetNbr = table.Column<int>(nullable: false),
+                    ZipCode = table.Column<int>(nullable: false),
+                    City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.LocationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sensor",
+                columns: table => new
+                {
+                    SensorId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    HexSensId = table.Column<string>(nullable: true),
+                    TreeSort = table.Column<string>(nullable: true),
+                    XCoordinate = table.Column<double>(nullable: false),
+                    YCoordinate = table.Column<double>(nullable: false),
+                    LocationId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sensor", x => x.SensorId);
+                    table.ForeignKey(
+                        name: "FK_Sensor_Location_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Location",
+                        principalColumn: "LocationId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tree",
+                columns: table => new
+                {
+                    TreeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TreeSort = table.Column<string>(nullable: true),
+                    Amount = table.Column<int>(nullable: false),
+                    LocationId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tree", x => x.TreeId);
+                    table.ForeignKey(
+                        name: "FK_Tree_Location_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Location",
+                        principalColumn: "LocationId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sensor_LocationId",
+                table: "Sensor",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tree_LocationId",
+                table: "Tree",
+                column: "LocationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sensor");
+
+            migrationBuilder.DropTable(
+                name: "Tree");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+        }
+    }
+}
